d74292579e226b17e4c002ca314f79809d11928d
Lucas <lucasmpx2000@gmail.com>
2017-08-01 01:09:36 -0300

    lab5

---
+++ lab5/src/Producer/simple_producer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+#!/usr/bin/env python
+import threading, logging, time
+import multiprocessing
+
+from kafka import KafkaConsumer, KafkaProducer
+
+
+class Producer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+        while True:
+            producer.send('my-topic', b"test")
+            producer.send('my-topic', b"Hello, world!")
+            time.sleep(1)
+
+
+class Consumer(multiprocessing.Process):
+    daemon = True
+
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+                                 auto_offset_reset='earliest')
+        consumer.subscribe(['my-topic'])
+
+        for message in consumer:
+            print (message)
+
+
+def main():
+    tasks = [
+        Producer(),
+        Consumer()
+    ]
+
+    for t in tasks:
+        t.start()
+
+    time.sleep(10)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+        )
+main()
